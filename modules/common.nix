223d86d5192307ab54bef85b2be22801dc25ffa0
RegRegLaptop <me@me>
2025-07-13 17:13:05 +0200

    58 current  2025-07-13 17:12:57  25.05.20250505.3730d8a     6.12.26                          * - Figuring out gc

--- modules/common.nix
+++ modules/common.nix
@@ -2,24 +2,8 @@
   config,
   pkgs,
   ...
-}: let
-  nixGcScript = ''
-    #!${pkgs.bash}/bin/bash
-    ${pkgs.nix}/bin/nix-collect-garbage --delete-older-than +10
-  '';
-in {
-  environment.etc."nix-gc-shutdown.sh".text = nixGcScript;
-
-  systemd.services.nix-gc-shutdown = {
-    description = "Run nix garbage collection before shutdown";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "/etc/nix-gc-shutdown.sh";
-      TimeoutStartSec = 0;
-      DefaultDependencies = false;
-      Before = ["shutdown.target"];
-      Conflicts = ["shutdown.target"]; # This ensures it runs before shutdown takes hold
-    };
-    wantedBy = ["shutdown.target"]; # This covers all types of shutdown, including desktop logout, etc.
-  };
+}: {
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc.dates = "weekly";
+  nix.gc.automatic = true;
 }
