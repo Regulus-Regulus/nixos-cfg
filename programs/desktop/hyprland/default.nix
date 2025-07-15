12797add8898f03351204430bda5b15521c85042
RegRegLaptop <me@me>
2025-07-15 21:41:03 +0200

    100 current  2025-07-15 21:40:54  25.05.20250505.3730d8a     6.12.26                          *

--- programs/desktop/hyprland/default.nix
+++ programs/desktop/hyprland/default.nix
@@ -90,24 +90,51 @@
           gaps_in = 4;
           gaps_out = 9;
           border_size = 2;
-          "col.active_border" = "0xFFFFECFD";
-          "col.inactive_border" = "0xFFEDFDFF";
+          "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
+          "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
           resize_on_border = true;
           layout = "master"; # dwindle or master
           # allow_tearing = true; # Allow tearing for games (use immediate window rules for specific games or all titles)
         };
+        animations = {
+          enabled = true;
+          bezier = [
+            "linear, 0, 0, 1, 1"
+            "md3_standard, 0.2, 0, 0, 1"
+            "md3_decel, 0.05, 0.7, 0.1, 1"
+            "md3_accel, 0.3, 0, 0.8, 0.15"
+            "overshot, 0.05, 0.9, 0.1, 1.1"
+            "crazyshot, 0.1, 1.5, 0.76, 0.92"
+            "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+            "fluent_decel, 0.1, 1, 0, 1"
+            "easeInOutCirc, 0.85, 0, 0.15, 1"
+            "easeOutCirc, 0, 0.55, 0.45, 1"
+            "easeOutExpo, 0.16, 1, 0.3, 1"
+          ];
+          animation = [
+            "windows, 1, 3, md3_decel, popin 60%"
+            "border, 1, 10, default"
+            "fade, 1, 2.5, md3_decel"
+            # "workspaces, 1, 3.5, md3_decel, slide"
+            "workspaces, 1, 3.5, easeOutExpo, slide"
+            # "workspaces, 1, 7, fluent_decel, slidefade 15%"
+            # "specialWorkspace, 1, 3, md3_decel, slidefadevert 15%"
+            "specialWorkspace, 1, 3, md3_decel, slidevert"
+          ];
+        };
         decoration = {
           shadow.enabled = false;
-          rounding = 5;
+          rounding = 8;
           dim_special = 0.3;
           blur = {
             enabled = true;
             special = true;
-            size = 6;
+            size = 10;
             passes = 3;
             new_optimizations = true;
             ignore_opacity = true;
             xray = false;
+            noise = 0.04;
           };
         };
         input = {
