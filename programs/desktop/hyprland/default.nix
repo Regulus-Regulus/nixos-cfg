da5eb6dd0c6d377df6324517c4295ded3c90a65e
RegRegLaptop <me@me>
2025-07-15 08:50:17 +0200

    94 current  2025-07-15 08:50:12  25.05.20250505.3730d8a     6.12.26                          *

--- programs/desktop/hyprland/default.nix
+++ programs/desktop/hyprland/default.nix
@@ -37,7 +37,7 @@
             "$mod, T, exec, kitty"
             "$mod, V, exec, codium"
             "$mod, Space, exec, rofi -show drun"
-            "$mod, Ctrl, Alt, Q, exec, poweroff"
+            "$mod Ctrl, Alt, Q, exec, poweroff"
           ]
           ++ (
             # workspaces
