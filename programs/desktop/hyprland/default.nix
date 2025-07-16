f524f1ca4d096ae4da76a09d5aebdab11edc0409
RegRegLaptop <me@me>
2025-07-16 20:08:23 +0200

    119 current  2025-07-16 20:08:13  25.05.20250505.3730d8a     6.12.26                          *

--- programs/desktop/hyprland/default.nix
+++ programs/desktop/hyprland/default.nix
@@ -65,29 +65,34 @@
             ])
             10));
         xwayland.force_zero_scaling = true;
-        env = [
-          "XDG_CURRENT_DESKTOP,Hyprland"
-          "XDG_SESSION_DESKTOP,Hyprland"
-          "XDG_SESSION_TYPE,wayland"
-          "GDK_BACKEND,wayland,x11,*"
-          "NIXOS_OZONE_WL,1"
-          "ELECTRON_OZONE_PLATFORM_HINT,auto"
-          "MOZ_ENABLE_WAYLAND,1"
-          "OZONE_PLATFORM,wayland"
-          "EGL_PLATFORM,wayland"
-          "CLUTTER_BACKEND,wayland"
-          "SDL_VIDEODRIVER,wayland"
-          "QT_QPA_PLATFORM,wayland;xcb"
-          "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-          "QT_QPA_PLATFORMTHEME,qt6ct"
-          "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-          # "WLR_RENDERER_ALLOW_SOFTWARE,1"
-          "NIXPKGS_ALLOW_UNFREE,1"
-
-          # mouse cursor
-          "XCURSOR_THEME,Bibata-Modern-Ice"
-          "XCURSOR_SIZE,24"
-        ];
+        home.sessionVariables = {
+          NIXOS_OZONE_WL = 1;
+          __GL_GSYNC_ALLOWED = 0;
+          __GL_VRR_ALLOWED = 0;
+          _JAVA_AWT_WM_NONEREPARENTING = 1;
+          SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
+          DISABLE_QT5_COMPAT = 0;
+          GDK_BACKEND = "wayland";
+          ANKI_WAYLAND = 1;
+          DIRENV_LOG_FORMAT = "";
+          WLR_DRM_NO_ATOMIC = 1;
+          QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+          QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+          QT_QPA_PLATFORM = "xcb";
+          QT_QPA_PLATFORMTHEME = "qt5ct";
+          QT_STYLE_OVERRIDE = "kvantum";
+          MOZ_ENABLE_WAYLAND = 1;
+          WLR_BACKEND = "vulkan";
+          WLR_RENDERER = "vulkan";
+          WLR_NO_HARDWARE_CURSORS = 1;
+          XDG_CURRENT_DESKTOP = "Hyprland";
+          XDG_SESSION_TYPE = "wayland";
+          XDG_SESSION_DESKTOP = "Hyprland";
+          SDL_VIDEODRIVER = "wayland";
+          CLUTTER_BACKEND = "wayland";
+          GTK_THEME = "Colloid-Green-Dark-Gruvbox";
+          GRIMBLAST_HIDE_CURSOR = 0;
+        };
         general = {
           gaps_in = 4;
           gaps_out = 9;
