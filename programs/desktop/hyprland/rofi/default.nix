48c7397ea2f3490111cceb11d422fd26cffa22f7
RegRegLaptop <me@me>
2025-07-16 18:41:21 +0200

    wip

--- programs/desktop/hyprland/rofi/default.nix
+++ programs/desktop/hyprland/rofi/default.nix
@@ -1,23 +1,38 @@
 {
-  self,
   pkgs,
+  lib,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    rofi-wayland
-  ];
   home-manager.sharedModules = [
     (_: {
-      programs.rofi = with pkgs; {
+      programs.rofi = {
         enable = true;
-        package = rofi-wayland;
+        package = pkgs.rofi-wayland;
         terminal = "kitty";
-        plugins = [
-          pkgs.rofi-emoji-wayland # https://github.com/Mange/rofi-emoji 🤯
-          pkgs.rofi-games # https://github.com/Rolv-Apneseth/rofi-games 🎮
+        plugins = with pkgs; [
+          rofi-emoji-wayland # https://github.com/Mange/rofi-emoji 🤯
+          rofi-games # https://github.com/Rolv-Apneseth/rofi-games 🎮
         ];
       };
-      xdg.configFile."rofi/themes/rounded-purple-dark.rasi".source = ./themes/rounded-purple-dark.rasi;
+      xdg.configFile."rofi/config-music.rasi".source = ./config-music.rasi;
+      xdg.configFile."rofi/config-long.rasi".source = ./config-long.rasi;
+      xdg.configFile."rofi/config-wallpaper.rasi".source = ./config-wallpaper.rasi;
+      xdg.configFile."rofi/launchers" = {
+        source = ./launchers;
+        recursive = true;
+      };
+      xdg.configFile."rofi/colors" = {
+        source = ./colors;
+        recursive = true;
+      };
+      xdg.configFile."rofi/assets" = {
+        source = ./assets;
+        recursive = true;
+      };
+      xdg.configFile."rofi/resolution" = {
+        source = ./resolution;
+        recursive = true;
+      };
     })
   ];
 }
