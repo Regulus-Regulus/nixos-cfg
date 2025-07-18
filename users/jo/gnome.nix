c6c5047e00bbe86b020b589476cb08876f19b0ab
RegRegLaptop <me@me>
2025-07-18 17:10:23 +0200

    generation-info-missing

--- users/jo/gnome.nix
+++ users/jo/gnome.nix
@@ -3,30 +3,35 @@
   pkgs,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.just-perfection
+    gnomeExtensions.arc-menu
+  ];
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    # settings = {
-    #   "org/gnome/shell" = {
-    #     disable-user-extensions = false; # enables user extensions
-    #     enabled-extensions = [
-    #       # Put UUIDs of extensions that you want to enable here.
-    #       # If the extension you want to enable is packaged in nixpkgs,
-    #       # you can easily get its UUID by accessing its extensionUuid
-    #       # field (look at the following example).
-    #       pkgs.gnomeExtensions.gsconnect.extensionUuid
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false; # enables user extensions
+        enabled-extensions = [
+          # Put UUIDs of extensions that you want to enable here.
+          # If the extension you want to enable is packaged in nixpkgs,
+          # you can easily get its UUID by accessing its extensionUuid
+          # field (look at the following example).
+          pkgs.gnomeExtensions.gsconnect.extensionUuid
 
-    #       # Alternatively, you can manually pass UUID as a string.
-    #       "blur-my-shell@aunetx"
-    #       # ...
-    #     ];
-    #   };
+          # Alternatively, you can manually pass UUID as a string.
+          # "blur-my-shell@aunetx"
+          # ...
+        ];
+      };
 
-    #   # Configure individual extensions
-    #   "org/gnome/shell/extensions/blur-my-shell" = {
-    #     brightness = 0.75;
-    #     noise-amount = 0;
-    #   };
-    # };
+      # Configure individual extensions
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        brightness = 0.75;
+        noise-amount = 0;
+      };
+    };
   };
 }
