eec9cf1aceae990a3be8119341059e32c582da59
RegRegLaptop <me@me>
2025-07-18 10:52:51 +0200

    generation-info-missing

--- modules/programs/desktop/gnome/default.nix
+++ modules/programs/desktop/gnome/default.nix
@@ -7,41 +7,70 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  # Disable unnecessary Gnome Auto-installs
-  programs.geary.enable = false;
-
   environment.systemPackages = with pkgs; [
     # gnomeExtensions.blur-my-shell
     # gnomeExtensions.just-perfection
     # gnomeExtensions.arc-menu
   ];
 
-  home-manager = {
-    dconf = {
-      enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      # settings = {
-      #   "org/gnome/shell" = {
-      #     disable-user-extensions = false; # enables user extensions
-      #     enabled-extensions = [
-      #       # Put UUIDs of extensions that you want to enable here.
-      #       # If the extension you want to enable is packaged in nixpkgs,
-      #       # you can easily get its UUID by accessing its extensionUuid
-      #       # field (look at the following example).
-      #       pkgs.gnomeExtensions.gsconnect.extensionUuid
+  environment.gnome.excludePackages = with pkgs; [
+    baobab # disk usage analyzer
+    # cheese # photo booth
+    # eog # image viewer
+    epiphany # web browser
+    gedit # text editor
+    simple-scan # document scanner
+    totem # video player
+    yelp # help viewer
+    evince # document viewer
+    # file-roller # archive manager
+    geary # email client
+    seahorse # password manager
 
-      #       # Alternatively, you can manually pass UUID as a string.
-      #       "blur-my-shell@aunetx"
-      #       # ...
-      #     ];
-      #   };
+    # these should be self explanatory
+    # gnome-calculator
+    # gnome-calendar
+    # gnome-characters
+    # gnome-clocks
+    # gnome-contacts
+    # gnome-font-viewer
+    # gnome-logs
+    # gnome-maps
+    # gnome-music
+    # gnome-photos
+    # gnome-screenshot
+    # gnome-system-monitor
+    # gnome-weather
+    # gnome-disk-utility
+    # pkgs.gnome-connections
+  ];
 
-      #   # Configure individual extensions
-      #   "org/gnome/shell/extensions/blur-my-shell" = {
-      #     brightness = 0.75;
-      #     noise-amount = 0;
-      #   };
-      # };
-    };
-  };
+  # homeSettings = {
+  #   dconf = {
+  #     enable = true;
+  #     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  #     # settings = {
+  #     #   "org/gnome/shell" = {
+  #     #     disable-user-extensions = false; # enables user extensions
+  #     #     enabled-extensions = [
+  #     #       # Put UUIDs of extensions that you want to enable here.
+  #     #       # If the extension you want to enable is packaged in nixpkgs,
+  #     #       # you can easily get its UUID by accessing its extensionUuid
+  #     #       # field (look at the following example).
+  #     #       pkgs.gnomeExtensions.gsconnect.extensionUuid
+
+  #     #       # Alternatively, you can manually pass UUID as a string.
+  #     #       "blur-my-shell@aunetx"
+  #     #       # ...
+  #     #     ];
+  #     #   };
+
+  #     #   # Configure individual extensions
+  #     #   "org/gnome/shell/extensions/blur-my-shell" = {
+  #     #     brightness = 0.75;
+  #     #     noise-amount = 0;
+  #     #   };
+  #     # };
+  #   };
+  # };
 }
