564986c219bbb5d0fe58c98387520597ef406061
RegRegLaptop <me@me>
2025-07-20 21:07:21 +0200

    generation-info-missing

--- users/jo/gnome.nix
+++ users/jo/gnome.nix
@@ -62,32 +62,11 @@
       cursor-theme = "Numix-Cursor";
     };
 
-    "org/gnome/desktop/wm/keybindings" = {
-      close = ["<Super>q"];
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-to-workspace-5 = ["<Super>5"];
-      switch-to-workspace-6 = ["<Super>6"];
-      switch-to-workspace-7 = ["<Super>7"];
-      switch-to-workspace-8 = ["<Super>8"];
-      switch-to-workspace-9 = ["<Super>9"];
-      switch-to-workspace-10 = ["<Super>0"];
-      switch-to-workspace-right = ["<Super>Right"];
-      switch-to-workspace-left = ["<Super>Left"];
-      move-to-workspace-1 = ["<Super><Alt>1"];
-      move-to-workspace-2 = ["<Super><Alt>2"];
-      move-to-workspace-3 = ["<Super><Alt>3"];
-      move-to-workspace-4 = ["<Super><Alt>4"];
-      move-to-workspace-5 = ["<Super><Alt>5"];
-      move-to-workspace-6 = ["<Super><Alt>6"];
-      move-to-workspace-7 = ["<Super><Alt>7"];
-      move-to-workspace-8 = ["<Super><Alt>8"];
-      move-to-workspace-9 = ["<Super><Alt>9"];
-      move-to-workspace-10 = ["<Super><Alt>0"];
-      move-to-workspace-right = ["<Super><Alt>Right"];
-      move-to-workspace-left = ["<Super><Alt>Left"];
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 10;
+    };
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
