6f217ada4f3e7ab02f35763c3edf264fecb18b3e
RegRegLaptop <me@me>
2025-09-17 10:14:24 +0200

    generation-info-missing

--- modules/programs/browser/librewolf/default.nix
+++ modules/programs/browser/librewolf/default.nix
@@ -5,81 +5,55 @@
 }: {
   environment.systemPackages = with pkgs; [pkgs.librewolf];
 
-  # home-manager.sharedModules = [
-  #   (_: {
-  #     programs.librewolf = {
-  #       enable = true;
-
-  #       policies = {
-  #         DisablePocket = true;
-  #         DisableTelemetry = true;
-  #         DisableFirefoxStudies = true;
-  #         PasswordManagerEnabled = false;
-  #         FirefoxHome = {
-  #           Pocket = false;
-  #           Snippets = false;
-  #         };
-  #       };
-
-  #       profiles.default = {
-  #         id = 0;
-  #         name = "default";
-
-  #         settings = {
-  #           # Search Engine
-  #           "browser.search.defaultenginename" = "Ecosia";
-  #           "browser.search.selectedEngine" = "Ecosia";
-  #           "browser.search.update" = false;
-
-  #           # Privacy & Tracking
-  #           "privacy.donottrackheader.enabled" = true;
-  #           "privacy.trackingprotection.enabled" = true;
-  #           "privacy.trackingprotection.socialtracking.enabled" = true;
-  #           "privacy.trackingprotection.cryptomining.enabled" = true;
-  #           # Breaks firefox sync login flow
-  #           "privacy.trackingprotection.fingerprinting.enabled" = false;
-  #           "privacy.partition.network_state.ocsp_cache" = true;
-
-  #           # HTTPS-Only Mode
-  #           "dom.security.https_only_mode" = true;
-
-  #           # Disable telemetry
-  #           "toolkit.telemetry.enabled" = false;
-  #           "toolkit.telemetry.unified" = false;
-  #           "toolkit.telemetry.archive.enabled" = false;
-  #           "toolkit.telemetry.updatePing.enabled" = false;
-  #           "datareporting.healthreport.uploadEnabled" = false;
-  #           "datareporting.policy.dataSubmissionEnabled" = false;
-
-  #           # Disable studies, normandy
-  #           "app.shield.optoutstudies.enabled" = false;
-  #           "app.normandy.enabled" = false;
-  #           "app.normandy.api_url" = "";
-
-  #           # Disable autofill (optional)
-  #           "signon.rememberSignons" = false;
-  #           "signon.autofillForms" = false;
-
-  #           # UI noise removal
-  #           "browser.newtabpage.activity-stream.feeds.snippets" = false;
-  #           "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-  #           "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-  #           "browser.newtabpage.activity-stream.showSponsored" = false;
-  #           "extensions.pocket.enabled" = false;
-  #           "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-
-  #           # Resist fingerprinting (some breakage possible)
-  #           "privacy.resistFingerprinting" = true;
-
-  #           # Disable geolocation prompts
-  #           "geo.enabled" = false;
-
-  #           # Disable webRTC IP leakage
-  #           "media.peerconnection.ice.default_address_only" = true;
-  #           "media.peerconnection.ice.no_host" = true;
-  #         };
-  #       };
-  #     };
-  #   })
-  # ];
+  home-manager.sharedModules = [
+    (_: {
+      programs.firefox = {
+        enable = true;
+        package = pkgs.librewolf;
+        policies = {
+          Cookies = {
+            "Allow" = [
+              "https://github.com"
+            ];
+            "Locked" = true;
+          };
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          ExtensionSettings = {
+            # BitWarden Password Manager
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/file/4567044/bitwarden_password_manager-2025.8.2.xpi";
+              installation_mode = "force_installed";
+            };
+            # uBlock Origin
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+            };
+          };
+          FirefoxHome = {
+            "Search" = false;
+          };
+          HardwareAcceleration = true;
+          Preferences = {
+            "browser.preferences.defaultPerformanceSettings.enabled" = false;
+            "browser.startup.homepage" = "about:home";
+            "browser.toolbar.bookmarks.visibility" = "newtab";
+            "browser.toolbars.bookmarks.visibility" = "newtab";
+            "browser.urlbar.suggest.bookmark" = false;
+            "browser.urlbar.suggest.engines" = false;
+            "browser.urlbar.suggest.history" = false;
+            "browser.urlbar.suggest.openpage" = false;
+            "browser.urlbar.suggest.recentsearches" = false;
+            "browser.urlbar.suggest.topsites" = false;
+            "browser.warnOnQuit" = false;
+            "browser.warnOnQuitShortcut" = false;
+            "places.history.enabled" = "false";
+            "privacy.resistFingerprinting" = true;
+            "privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts" = true;
+          };
+        };
+      };
+    })
+  ];
 }
