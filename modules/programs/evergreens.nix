0b1f0fd356924cdbc132dd1580b7be78042a1ec4
RegRegLaptop <me@me>
2025-10-11 20:09:39 +0200

    generation-info-missing

--- modules/programs/evergreens.nix
+++ modules/programs/evergreens.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
+  services.flatpak.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
