0454091e1d91ed5b15b48b104cb49cc6a8d3c495
RegRegLaptop <me@me>
2025-10-17 11:01:49 +0200

    adding katharina

---
+++ users/katharina/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  lib,
+  config,
+  hostConfigName,
+  ...
+}: {
+  homeSettings = {
+    home = {
+      username = "katharina";
+      homeDirectory = "/home/katharina";
+      stateVersion = "24.11";
+      packages = with pkgs;
+        builtins.concatLists [
+          # Always install
+          [
+          ]
+
+          # Just on the laptop
+          (lib.optionals (hostConfigName == "laptop") [
+            ])
+
+          # Only desktop
+          (lib.optionals (hostConfigName == "desktop") [
+            ])
+        ];
+    };
+
+    programs = {
+      home-manager.enable = true;
+
+      fish = {
+        enable = false;
+      };
+
+      zsh = {
+        enable = true;
+        # Overrides und Ergänzungen zum Default:
+        shellAliases = {
+          # z.B. eigene Aliase, die zum Default hinzukommen
+          ll = "${pkgs.eza}/bin/eza -lha --icons=auto";
+          ls = "${pkgs.eza}/bin/eza -1 --icons=auto";
+        };
+        oh-my-zsh.plugins = lib.mkForce [
+          "docker" # zusätzlich zum Default "git", "gitignore", "z"
+        ];
+      };
+    };
+  };
+
+  systemSettings = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = ["wheel"];
+  };
+}
