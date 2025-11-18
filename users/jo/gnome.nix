b08fee87830c2eb16bf32685dd8df8aa80afb1cf
regulus-regulus <regulus-regulus@posteo.de>
2025-11-18 20:10:21 +0100

    generation-info-missing

--- users/jo/gnome.nix
+++ users/jo/gnome.nix
@@ -65,7 +65,7 @@
     };
 
     "org/gnome/desktop/wm/keybindings" = {
-      move-to-workspace-left = [];
+      move-to-workspace-left = "disabled";
       move-to-workspace-right = [];
       move-to-workspace-up = [];
       move-to-workspace-down = [];
