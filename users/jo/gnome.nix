c472bcc7983d4fcd586bf9711dd49743527dab15
regulus-regulus <regulus-regulus@posteo.de>
2025-11-18 21:55:14 +0100

    generation-info-missing

--- users/jo/gnome.nix
+++ users/jo/gnome.nix
@@ -19,82 +19,74 @@
     };
   };
 
-  dconf.enable = true;
+  dconf = {
+    enable = true;
 
-  dconf.settings = let
-    cbPrefix = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings";
-    makeBinding = name: cfg: {
-      "${cbPrefix}/${name}/" = cfg;
-    };
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        icon-theme = "Papirus-Dark";
+        cursor-theme = "Numix-Cursor";
+      };
 
-    customBindings = {
-      custom0 = {
+      "org/gnome/shell/extensions/paperwm/keybindings" = {
+        "switch-down-workspace" = "<Super>s";
+        "switch-up-workspace" = "<Super>w";
+        "switch-right" = "<Super>d";
+        "switch-left" = "<Super>a";
+        "restore-keybinds" = "";
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         name = "Kitty Terminal";
         command = "kitty";
         binding = "<Ctrl><Alt>t";
       };
 
-      custom1 = {
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
         name = "Librewolf";
         command = "librewolf";
         binding = "<Super>b";
       };
-    };
 
-    # Merge them into a single attrset with full paths
-    customBindingAttrs = builtins.foldl' (
-      acc: name:
-        acc // (makeBinding name (customBindings.${name}))
-    ) {} (builtins.attrNames customBindings);
-  in {
-    # "org/gnome/desktop/interface" = {
-    #   color-scheme = "prefer-dark";
-    #   icon-theme = "Papirus-Dark";
-    #   cursor-theme = "Numix-Cursor";
-    # };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        ];
+      };
 
-    "org/gnome/shell/extensions/paperwm/keybindings" = {
-      "switch-down-workspace" = "<Super>s";
-      "switch-up-workspace" = "<Super>w";
-      "switch-right" = "<Super>d";
-      "switch-left" = "<Super>a";
-      "restore-keybinds" = "";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = builtins.map (name: "/${cbPrefix}/${name}/") (builtins.attrNames customBindings);
-    };
+      "org/gnome/desktop/wm/keybindings" = {
+        move-to-workspace-left = [];
+        move-to-workspace-right = [];
+        move-to-workspace-up = [];
+        move-to-workspace-down = [];
 
-    "org/gnome/desktop/wm/keybindings" = {
-      move-to-workspace-left = [];
-      move-to-workspace-right = [];
-      move-to-workspace-up = [];
-      move-to-workspace-down = [];
+        switch-to-workspace-up = [];
+        switch-to-workspace-down = [];
+        switch-to-workspace-left = [];
+        switch-to-workspace-right = [];
 
-      switch-to-workspace-up = [];
-      switch-to-workspace-down = [];
-      switch-to-workspace-left = [];
-      switch-to-workspace-right = [];
+        # Window movement
+        move-left = [];
+        move-right = [];
+      };
 
-      # Window movement
-      move-left = [];
-      move-right = [];
-    };
+      "org/gnome/shell/keybindings" = {
+        toggle-overview = []; # unbinds <Super>s
+        toggle-application-view = []; # unbinds <Super>
+      };
 
-    "org/gnome/shell/keybindings" = {
-      toggle-overview = []; # unbinds <Super>s
-      toggle-application-view = []; # unbinds <Super>
-    };
+      # "org/gnome/shell/extensions/user-theme" = {
+      #   name = "Gruvbox-dark";
+      # };
 
-    # "org/gnome/shell/extensions/user-theme" = {
-    #   name = "Gruvbox-dark";
-    # };
-
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [
-        pkgs.gnomeExtensions.user-themes.extensionUuid
-        pkgs.gnomeExtensions.paperwm.extensionUuid
-      ];
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          pkgs.gnomeExtensions.user-themes.extensionUuid
+          pkgs.gnomeExtensions.paperwm.extensionUuid
+        ];
+      };
     };
   };
 }
