a5a4211f7cebe4e2d09249c8f09ac5eca044f4cc
regulus-regulus <regulus-regulus@posteo.de>
2025-11-18 18:15:15 +0100

    generation-info-missing

--- users/jo/gnome.nix
+++ users/jo/gnome.nix
@@ -81,6 +81,7 @@
 
     "org/gnome/shell/keybindings" = {
       toggle-overview = []; # unbinds <Super>s
+      toggle-application-view = []; # unbinds <Super>
     };
 
     # "org/gnome/shell/extensions/user-theme" = {
