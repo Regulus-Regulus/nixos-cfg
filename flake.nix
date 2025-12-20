ced388ef6e0a3e8b27e11e8ee0205e3190f549dd
regulus-regulus <regulus-regulus@posteo.de>
2025-12-20 16:29:09 +0100

    generation-info-missing

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-generators = {
