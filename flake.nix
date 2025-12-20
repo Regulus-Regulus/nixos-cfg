6171feecd9a4f7dc2794963d8b64421be2f3f055
regulus-regulus <regulus-regulus@posteo.de>
2025-12-20 16:28:26 +0100

    generation-info-missing

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
