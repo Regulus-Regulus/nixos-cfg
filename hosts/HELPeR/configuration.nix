cbcf33220a25c3e49abe8fc76cbc5c5f6164b628
regulus-regulus <regulus-regulus@posteo.de>
2025-12-27 13:56:36 +0100

    added pihole-ftl to helper

--- hosts/HELPeR/configuration.nix
+++ hosts/HELPeR/configuration.nix
@@ -126,6 +126,14 @@
     };
   };
 
+  services.pihole-ftl = {
+    enabled = true;
+    queryLogDeleter = {
+      enabled = true;
+      age = 30;
+    }
+  }
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
