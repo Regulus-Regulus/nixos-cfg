4628d4b50ed1c73a3d5b0a689e959cc341dcdd62
RegRegLaptop <me@me>
2026-04-23 17:53:29 +0200

    wip

--- home/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  imports = [
-    ./programs/browser/firefox.nix
-  ];
-}
---
+++ home/users/jo/default.nix
@@ -0,0 +1,74 @@
+{
+  pkgs,
+  lib,
+  hostConfigName,
+  ...
+}: {
+  users.users.jo = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = ["wheel"];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrd6LHfR66IA+p+40RyIqGtYqFrcdf09p5REwTjkWZW jo@rr-desktop"
+    ];
+  };
+
+  home-manager.users.jo = {
+    imports = [
+      ./gnome.nix
+      ./hyfetch.nix
+      ./stylix.nix
+    ];
+
+    my.home.programs.firefox.enable = true;
+
+    home = {
+      username = "jo";
+      homeDirectory = "/home/jo";
+      stateVersion = "24.11";
+
+      packages = with pkgs;
+        builtins.concatLists [
+          # Always
+          [
+            iw
+            gnupg
+            thunderbird
+            wget
+          ]
+
+          # Laptop or Desktop
+          (lib.optionals (hostConfigName == "laptop" || hostConfigName == "desktop") [
+            nmap
+            wireshark
+            discord
+            nmap
+            obsidian
+            traceroute
+          ])
+        ];
+    };
+
+    programs = {
+      home-manager.enable = true;
+
+      zsh = {
+        enable = true;
+
+        shellAliases = {
+          ll = "${pkgs.eza}/bin/eza -lha --icons=auto";
+          ls = "${pkgs.eza}/bin/eza -1 --icons=auto";
+        };
+
+        oh-my-zsh.plugins = lib.mkForce [
+          "docker"
+        ];
+
+        sessionVariables = {
+          NIX_SSHOPTS = "-p 7373";
+        };
+      };
+    };
+  };
+}
