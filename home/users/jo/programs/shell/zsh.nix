8d836ffc47060cea4de0e64fccf20ba775199318
RegRegLaptop <me@me>
2026-05-26 21:20:07 +0200

    generation-info-missing - q

--- home/users/jo/programs/shell/zsh.nix
+++ home/users/jo/programs/shell/zsh.nix
@@ -21,7 +21,7 @@ in {
         size = 100000;
         path = "$HOME/.local/share/zsh/history";
       };
-      dotDir = config.home.homeDirectory;
+      dotDir = ".config/zsh";
       # initContent = ''
       #   source ${pkgs.spaceship-prompt}/share/zsh/themes/spaceship.zsh-theme;
       # '';
